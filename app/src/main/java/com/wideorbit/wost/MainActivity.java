04af621d9568d192225309e4d8f85a7150ad3250
David Wright <dwright@wideorbit.com>
2019-09-30 13:05:41 -0700

    Fix type and remove dup

--- app/src/main/java/com/wideorbit/wost/MainActivity.java
+++ app/src/main/java/com/wideorbit/wost/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		WOSTLibrary wost = new WOSTLibrary();
 		System.out.println(wost.output());
 	}
